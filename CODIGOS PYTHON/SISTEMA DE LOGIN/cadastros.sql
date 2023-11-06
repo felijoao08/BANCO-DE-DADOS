94c9ef44a8f8042008184d42a55fb2094fd551b7
Felijoao08 <felipe.joao1@escolar.ifrn.edu.br>
2023-11-05 21:47:15 -0300

    ok

---
+++ CODIGOS PYTHON/SISTEMA DE LOGIN/cadastros.sql
@@ -0,0 +1,11 @@
+
+USE cadastros;
+/*
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    senha VARCHAR(10) NOT NULL
+);
+*/
+
+SELECT nome, senha FROM usuarios;
--- CODIGOS PYTHON/SISTEMA DE LOGIN/login.py
+++ CODIGOS PYTHON/SISTEMA DE LOGIN/login.py
@@ -1,30 +1,78 @@
-from flask import Flask,  request, jsonify
+from flask import Flask,  request, jsonify, render_template, redirect, flash
 
 import mysql.connector
  
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "dsafonfnwruihruiwfjlhbsdavbieffui"
 
 db = mysql.connector.connect (
     host = 'localhost',
     user = "root",
-    password = 'labinfo',
-    database = "atividade"
+    password = 'felipe09',
+    database = "cadastros"
 )
 
-@app.route('/login', method =['POST']) 
-def login():
-    data = request.json
+#PAGINA INICIAL 
+@app.route('/')
+def homepage():
+   return render_template('homepage.html')
+
+
+#ROTA PARA O CADASTRO DE PESSOAS
+@app.route('/cadastro')
+def homecadastro():
+    return render_template('index.html')
+@app.route('/cadastro', methods=['POST'])
+def cadastro():
+    usuario = request.form.get('usuario')
+    Password = request.form.get('Password')
+
+    #aqui irei cadastrar os dados no bando de dados
     cursor = db.cursor(dictionary=True)
-
-    cursor.execute('SELECT * FROM usuario WHERE email = %s AND semha = %s, (data['email'], data[senha])')
-    user = cursor.fetchone()
+    cursor.execute("INSERT INTO usuarios (nome, senha) VALUES (%s, %s)", (usuario, Password))
+    db.commit()
     cursor.close()
+    return redirect('/resposta')
 
-    if user:
-        return jsonify({"login bem sucedido!"})
-    else:
-        return jsonify({"email ou senha incorreta!"})
+
+#ROTA PARA LOGIN(VERIFICAR SE ESTAR CADASTRADO)
+@app.route('/login')
+def verificarlogin():
+    return render_template('login.html')
+@app.route('/login', methods=['POST'])
+def login():
+    usuario = request.form.get('usuario')
+    Password = request.form.get('Password')
+
+    #aqui irei verificar se o usuario esta cadastrado ou nao
+    cursor = db.cursor(dictionary=True)
+    cursor.execute('SELECT nome, senha FROM usuarios')
+    objetos =  cursor.fetchall()
+
+    #pegando os valores que quero do dicionario(o banco de dados)
+    name = None
+    senha = None
+    for obj in objetos:
+        name = obj.get('nome')
+        senha = obj.get('senha')
+    
+    #verificando se os valores sao correspondentes 
+    if name == usuario and senha == Password:
+        cursor.close()
+        return redirect('/resposta')
         
+    else:
+        flash("Invalid password or user")
+        cursor.close()
+        return redirect('/login')
+        
+    
+        
+
+#ROTA PARA QUANDO ENTRAR NO SITE
+@app.route('/resposta')
+def resposta():
+    return render_template('resposta.html')
+
 if __name__ == '__main__':
     app.run()
-    
