82f557ee41cd6a1c546b7d3ce0d730ca6cd68d98
Felijoao08 <felipe.joao1@escolar.ifrn.edu.br>
2023-11-20 22:47:14 -0300

    ok

--- eNAPNE testes/BD-eNAPNE/Pesquisas_eNAPNE.sql
+++ eNAPNE testes/BD-eNAPNE/Pesquisas_eNAPNE.sql
@@ -92,32 +92,6 @@ WHERE
 
 
 
-SELECT
-	curso.nome AS nome_curso,
-	curso.nivelEnsino AS Nive_de_Ensino,
-    curso.coordenador AS coordenador,
-    aluno.nome AS nome_aluno,
-    aluno.necessidadesEspecificas AS necessidades_Especificas,
-    aluno.matricula As matricula,
-    equipeMultiDisciplinar.nome AS Equipe_nome,
-    equipeMultiDisciplinar.funcao AS funcao, 
-    anexo1.historico AS historico,
-    anexo1.conhecimentosHabilidades AS conhecimentos_habilidades,
-    anexo1.dificuldades As dificuldades,
-    anexo1.observacoes AS observacoes,
-    anexo2.disciplina AS disciplina,
-    anexo2.docente AS docente,
-	anexo2.objetivosPlanoDisciplina AS OPD,
-    anexo2.objetivosAdaptacoesDisciplina AS OAD,
-    anexo2.conteudoPlanoDisciplina AS CPD,
-    anexo2.conteudoAdaptacoesDisciplina AS CAD,
-    anexo2.metodologiaPlanoDisciplina AS MPD, 
-    anexo2.metodologiaAdaptacoesDisciplina AS MAD,
-    anexo2.recursoDidaticoPlanoDisciplina AS RDPD,
-    anexo2.recursoDidaticoAdaptacoesDisciplina AS RDAD, 
-    anexo2.avaliacaoPlanoDisciplina AS APD,
-    anexo2.avaliacaoAdaptacoesDisciplina AS AAD,
-    anexo3.avancos AS avancos,
-    anexo3.parecer AS parecer,
+SELECT curso.nome AS nome_curso, curso.nivelEnsino AS Nive_de_Ensino, curso.coordenador AS coordenador, aluno.nome AS nome_aluno, aluno.necessidadesEspecificas AS necessidades_Especificas, aluno.matricula As matricula, equipeMultiDisciplinar.nome AS Equipe_nome, equipeMultiDisciplinar.funcao AS funcao, anexo1.historico AS historico, anexo1.conhecimentosHabilidades AS conhecimentos_habilidades, anexo1.dificuldades As dificuldades, anexo1.observacoes AS observacoes, anexo2.disciplina AS disciplina, anexo2.docente AS docente, anexo2.objetivosPlanoDisciplina AS OPD, anexo2.objetivosAdaptacoesDisciplina AS OAD, anexo2.conteudoPlanoDisciplina AS CPD, anexo2.conteudoAdaptacoesDisciplina AS CAD, anexo2.metodologiaPlanoDisciplina AS MPD,  anexo2.metodologiaAdaptacoesDisciplina AS MAD, anexo2.recursoDidaticoPlanoDisciplina AS RDPD, anexo2.recursoDidaticoAdaptacoesDisciplina AS RDAD, anexo2.avaliacaoPlanoDisciplina AS APD, anexo2.avaliacaoAdaptacoesDisciplina AS AAD, anexo3.avancos AS avancos, anexo3.parecer AS parecer FROM curso INNER JOIN aluno ON aluno.codigoCurso = curso.codigo INNER JOIN pei ON aluno.matricula = pei.matriculaAluno INNER JOIN equipeMultidisciplinar ON equipeMultidisciplinar.numeroPei = pei.numero INNER JOIN anexo1 ON anexo1.numeroPei = pei.numero INNER JOIN anexo2 ON anexo2.numeroPei = pei.numero INNER JOIN anexo3 ON anexo3.numeroPei = pei.numero WHERE aluno.matricula = '99999999'
     
     
